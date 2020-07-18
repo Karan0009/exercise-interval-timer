13332211d7d66db354d675ff5db425e82d898e9f
Karan0009 <karansinghk0@gmail.com>
2020-07-18 19:11:57 +0530

    sounds added

---
+++ src/pages/settings/sounds/sounds.js
@@ -0,0 +1,7 @@
+import sound1 from "./bell-start.mp3";
+import sound2 from "./block-start.mp3";
+
+export const sounds = [
+  { name: "bell-start", src: sound1 },
+  { name: "block-start", src: sound2 },
+];
